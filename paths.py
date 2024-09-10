b2514b1a008e78605bda58da35774105878244b9
Omri Yakir <omriyakir@rack-wolfson-g01.cs.tau.ac.il>
2024-09-10 14:08:20 +0300

    new paths for find orthologs task

--- paths.py
+++ paths.py
@@ -9,11 +9,25 @@ data_path = os.path.join(current_dir, 'data')
 juno_path = os.path.join(data_path, 'Juno')
 #   Izumo
 izumo_path = os.path.join(data_path, 'Izumo')
+#   human_proteins_db
+human_proteins_db_path = os.path.join(data_path, 'human_proteins_db')
 
 
 #conservation_analysis
 conservation_analysis_path = os.path.join(current_dir, 'conservation_analysis')
 #   Juno
 juno_conservation_path = os.path.join(conservation_analysis_path, 'Juno')
+#       orthologs
+juno_orthologs_path = os.path.join(juno_conservation_path, 'orthologs')
+#           fasta_files_to_align
+juno_fasta_files_to_align_path = os.path.join(juno_orthologs_path, 'fasta_files_to_align')
+#           blast_results
+juno_blast_results_path = os.path.join(juno_orthologs_path, 'blast_results')
 #   Izumo
 izumo_conservation_path = os.path.join(conservation_analysis_path, 'Izumo')
+#       orthologs
+izumo_orthologs_path = os.path.join(izumo_conservation_path, 'orthologs')
+#           fasta_files_to_align
+izumo_fasta_files_to_align_path = os.path.join(izumo_orthologs_path, 'fasta_files_to_align')
+#           blast_results
+izumo_blast_results_path = os.path.join(izumo_orthologs_path, 'blast_results')
